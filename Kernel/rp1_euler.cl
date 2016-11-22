150a61a465099e7e83c17b2e2372ed138babee1e
YuHsuan Shih <yoyoshih13@gmail.com>
2016-11-21 23:50:01 -0500

    finished riemann solver for euler equation

--- Kernel/rp1_euler.cl
+++ Kernel/rp1_euler.cl
@@ -1,11 +1,13 @@
-__kernel void rp1_acoustic(__global float* d_q, 
-                           __global float* d_apdq, 
-                           __global float* d_amdq,
-                           __global float* d_s,
-                           const int mx, const int mbc, 
-                           const float rho, const float K)
+__kernel void rp1_euler(__global float* d_q, 
+                        __global float* d_apdq, 
+                        __global float* d_amdq,
+                        __global float* d_s,
+                        const int mx, const int mbc, 
+                        const float gamma)
 {
-    /* Input: q, Output: d_apdq, d_amdq*/
+    /* Input: q, Output: d_apdq, d_amdq, d_s */
+    /* meqn = 3, mwaves = 3 */
+    /* Conserved quantities: density(rho), momentum(rhou), energy(e) */
     int i = get_global_id(0); 
     d_s[i] = 0;
     if (i < mx + mbc + 1 && i > mbc - 1)
@@ -13,32 +15,90 @@ __kernel void rp1_acoustic(__global float* d_q,
         /* Solve riemann problem from "ith" cell boundary 
           (the boundary of ith and (i-1)th cell) */
         
-        float pl = d_q[2*(i-1)], ul = d_q[2*(i-1)+1], 
-              pr = d_q[2*i], ur = d_q[2*i+1];
+        float rhol = d_q[3*(i-1)], rhoul = d_q[3*(i-1)+1], el = d_q[3*(i-1)+2],
+              rhor = d_q[3*i], rhour = d_q[3*i+1], er = d_q[3*i+2];
 
-        float cc = sqrt(K/rho), zz = cc*rho;
-        float delta[2], 
-              a1, s1, wave1[2], 
-              a2, s2, wave2[2]; 
+        float gamma1 = gamma - 1.0;
+        float delta[3], 
+              a1, s1, wave1[3], 
+              a2, s2, wave2[3], 
+              a3, s3, wave3[3]; 
+        float u, a, enth;
+        float rhosqrtl, rhosqrtr, pl, pr, rhosq2;
+        float temp;
 
-        delta[0] = pr - pl;
-        delta[1] = ur - ul;
+        /* Calculate roe average */
+        rhosqrtl = sqrt(rhol);
+        rhosqrtr = sqrt(rhor);
+        pl = gamma1*(el - 0.5*pow(rhoul, 2)/rhol);
+        pr = gamma1*(er - 0.5*pow(rhour, 2)/rhor);
+        rhosq2 = rhosqrtl + rhosqrtr;
+        u = (rhoul/rhosqrtl + rhour/rhosqrtr)/rhosq2;
+        enth = ((el + pl)/rhosqrtl + (er + pr)/rhosqrtr)/rhosq2;
+        a = sqrt(gamma1*(enth - 0.5*pow(u, 2)));
         
-        a1 = (-delta[0] + zz*delta[1]) / (2.0*zz);
-        wave1[0] = -a1*zz;
-        wave1[1] = a1;
-        s1 = -cc;
-        d_amdq[2*i]   = s1*wave1[0];
-        d_amdq[2*i+1] = s1*wave1[1];
+        /* find coefficients of the 3 eigenvectors */
+        delta[0] = rhor - rhol;
+        delta[1] = rhour - rhoul;
+        delta[2] = er - el;
+        a2 = gamma1/pow(a, 2) * ((enth - pow(u, 2))*delta[0] + u*delta[1] - delta[2]);
+        a3 = (delta[1] + (a-u)*delta[0] - a*a2)/(2.0*a);
+        a1 = delta[0] - a2 - a3;
+
+        /* Compute the waves */
+        wave1[0] = a1;
+        wave1[1] = a1 * (u-a);
+        wave1[2] = a1 * (enth - u*a);
+        s1 = u - a;
+
+        wave2[0] = a2;
+        wave2[1] = a2 * u;
+        wave2[2] = a2 * 0.5 * pow(u, 2);
+        s2 = u;
+
+        wave3[0] = a3;
+        wave3[1] = a3 * (u+a);
+        wave3[2] = a3 * (enth + u*a);
+        s3 = u + a;
+
+        /* Compute Godunov flux */
+        d_amdq[3*i]   = 0.0;
+        d_amdq[3*i+1] = 0.0;
+        d_amdq[3*i+2] = 0.0;
+        d_apdq[3*i]   = 0.0;
+        d_apdq[3*i+1] = 0.0;
+        d_apdq[3*i+2] = 0.0;
         
-        a2 = (delta[0] + zz*delta[1]) / (2.0*zz);
-        wave2[0] = a2*zz;
-        wave2[1] = a2;
-        s2 = cc;
+        if (s1 < 0){
+            d_amdq[3*i]   += s1*wave1[0];
+            d_amdq[3*i+1] += s1*wave1[1];
+            d_amdq[3*i+2] += s1*wave1[2];
+        }else{
+            d_apdq[3*i]   += s1*wave1[0];
+            d_apdq[3*i+1] += s1*wave1[1];
+            d_apdq[3*i+2] += s1*wave1[2];
+        }
 
-        d_apdq[2*i]   = s2*wave2[0];
-        d_apdq[2*i+1] = s2*wave2[1];
+        if (s2 < 0){
+            d_amdq[3*i]   += s2*wave2[0];
+            d_amdq[3*i+1] += s2*wave2[1];
+            d_amdq[3*i+2] += s2*wave2[2];
+        }else{
+            d_apdq[3*i]   += s2*wave2[0];
+            d_apdq[3*i+1] += s2*wave2[1];
+            d_apdq[3*i+2] += s2*wave2[2];
+        }
 
-        d_s[i] = max(-s1, s2);
+        if (s3 < 0){
+            d_amdq[3*i]   += s3*wave3[0];
+            d_amdq[3*i+1] += s3*wave3[1];
+            d_amdq[3*i+2] += s3*wave3[2];
+        }else{
+            d_apdq[3*i]   += s3*wave3[0];
+            d_apdq[3*i+1] += s3*wave3[1];
+            d_apdq[3*i+2] += s3*wave3[2];
+        }
+        temp = max(fabs(s1), fabs(s2));
+        d_s[i] = max(temp, fabs(s3));
     }
 }
--- euler_v2.cpp
+++ euler_v2.cpp
@@ -19,44 +19,46 @@ int main(int argc, char const *argv[])
     cl_device_id      device = GetDevice(platform, 0);
     cl_context        context;
     cl_command_queue  commands;
-    cl_program        p_rp1_acoustic, p_qinit, p_bc1, 
+    cl_program        p_rp1_euler, p_qinit, p_bc1, 
                       p_update_q1, p_max_speed;
-    cl_kernel         k_rp1_acoustic, k_qinit, k_bc1, 
+    cl_kernel         k_rp1_euler, k_qinit, k_bc1, 
                       k_update_q1, k_max_speed;
     cl_mem            d_q, d_q_old, d_apdq, d_amdq, d_s;
 
     /* Data for pde solver */
-    int meqn = 2, mwaves = 2, maux = 0;
+    int meqn = 3, mwaves = 3, maux = 0;
     int ndim = 1;
-    int mx = 100, mbc = 2, mtot = mx + 2*mbc;
-    int nout = 16;
+    int mx = 800, mbc = 2, mtot = mx + 2*mbc;
+    int nout = 10;
     int iframe = 0;
     
     /* physical domain */
-    float xlower = -1.0, xupper = 1.0;
+    float xlower = 0.0, xupper = 1.0;
     float dx = (xupper - xlower)/mx;
     
     /* time */
-    int maxtimestep = 1000;
+    int maxtimestep = 10000;
     float t = 0, t_old;
-    float t_start = 0, t_final = 1.0;
-    float dt = dx/2, dtmax = 1.0, dtmin = 0.0;
+    float t_start = 0, t_final = 0.038;
+    float dt = 0.00002, dtmax = 1.0, dtmin = 0.0;
     float dtout = t_final/nout, tout = 0;
     
     /* data */
     float q[meqn*mtot];
-
+    float s[meqn*mtot];
     /* problem data */
-    float K = 4.0, rho = 1.0;
+    float gamma = 1.4;
     
     /* other */
     float cfl, cflmax = 1, cfldesire = 0.9;
-
-    std::size_t local    = mtot/2;
+    char outdir[] = "Output/euler";
+    
+    std::size_t local    = mtot/4;
     std::size_t numgroup = ((mtot - 1)/local + 1);
     std::size_t global   = numgroup * local;
     std::size_t l;
 
+    std::cout<<"local = "<<local<<"; global = "<<global<<std::endl;
     /* Create context */
     context = clCreateContext(0, 1, &device, NULL, NULL, &err);
     CheckError(err);
@@ -66,13 +68,13 @@ int main(int argc, char const *argv[])
     CheckError(err);
 
     /* Create program, kernel from source */
-    p_rp1_acoustic = CreateProgram(LoadKernel ("Kernel/rp1_acoustic.cl"), context);
-    err     = clBuildProgram(p_rp1_acoustic, 1, &device, NULL, NULL, NULL);
+    p_rp1_euler = CreateProgram(LoadKernel ("Kernel/rp1_euler.cl"), context);
+    err     = clBuildProgram(p_rp1_euler, 1, &device, NULL, NULL, NULL);
     if (err != CL_SUCCESS)
     {
-        ProgramErrMsg(p_rp1_acoustic, device);
+        ProgramErrMsg(p_rp1_euler, device);
     }
-    k_rp1_acoustic = clCreateKernel(p_rp1_acoustic, "rp1_acoustic", &err);
+    k_rp1_euler = clCreateKernel(p_rp1_euler, "rp1_euler", &err);
 
     p_update_q1 = CreateProgram(LoadKernel ("Kernel/update_q1.cl"), context);
     err     = clBuildProgram(p_update_q1, 1, &device, NULL, NULL, NULL);
@@ -91,13 +93,13 @@ int main(int argc, char const *argv[])
     k_max_speed = clCreateKernel(p_max_speed, "max_speed", &err);
 
 
-    p_qinit = CreateProgram(LoadKernel ("Kernel/qinit.cl"), context);
+    p_qinit = CreateProgram(LoadKernel ("Kernel/euler_qinit.cl"), context);
     err     = clBuildProgram(p_qinit, 1, &device, NULL, NULL, NULL);
     if (err != CL_SUCCESS)
     {
         ProgramErrMsg(p_qinit, device);
     }
-    k_qinit = clCreateKernel(p_qinit, "qinit", &err);
+    k_qinit = clCreateKernel(p_qinit, "euler_qinit", &err);
     CheckError(err);
 
     p_bc1 = CreateProgram(LoadKernel ("Kernel/bc1.cl"), context);
@@ -124,6 +126,7 @@ int main(int argc, char const *argv[])
     CheckError(clSetKernelArg(k_qinit, 3, sizeof(int)   , &mbc));
     CheckError(clSetKernelArg(k_qinit, 4, sizeof(float) , &xlower));
     CheckError(clSetKernelArg(k_qinit, 5, sizeof(float) , &dx));
+    CheckError(clSetKernelArg(k_qinit, 6, sizeof(float) , &gamma));
 
     /* BC1 */
     CheckError(clSetKernelArg(k_bc1, 0, sizeof(cl_mem), &d_q));
@@ -132,14 +135,13 @@ int main(int argc, char const *argv[])
     CheckError(clSetKernelArg(k_bc1, 3, sizeof(int)   , &mbc));
 
     /* RP1 */
-    CheckError(clSetKernelArg(k_rp1_acoustic, 0, sizeof(cl_mem), &d_q));
-    CheckError(clSetKernelArg(k_rp1_acoustic, 1, sizeof(cl_mem), &d_apdq));
-    CheckError(clSetKernelArg(k_rp1_acoustic, 2, sizeof(cl_mem), &d_amdq));
-    CheckError(clSetKernelArg(k_rp1_acoustic, 3, sizeof(cl_mem), &d_s));
-    CheckError(clSetKernelArg(k_rp1_acoustic, 4, sizeof(int)   , &mx));
-    CheckError(clSetKernelArg(k_rp1_acoustic, 5, sizeof(int)   , &mbc));
-    CheckError(clSetKernelArg(k_rp1_acoustic, 6, sizeof(float) , &rho));
-    CheckError(clSetKernelArg(k_rp1_acoustic, 7, sizeof(float) , &K));
+    CheckError(clSetKernelArg(k_rp1_euler, 0, sizeof(cl_mem), &d_q));
+    CheckError(clSetKernelArg(k_rp1_euler, 1, sizeof(cl_mem), &d_apdq));
+    CheckError(clSetKernelArg(k_rp1_euler, 2, sizeof(cl_mem), &d_amdq));
+    CheckError(clSetKernelArg(k_rp1_euler, 3, sizeof(cl_mem), &d_s));
+    CheckError(clSetKernelArg(k_rp1_euler, 4, sizeof(int)   , &mx));
+    CheckError(clSetKernelArg(k_rp1_euler, 5, sizeof(int)   , &mbc));
+    CheckError(clSetKernelArg(k_rp1_euler, 6, sizeof(float) , &gamma));
 
     /* UPDATE_Q */
     CheckError(clSetKernelArg(k_update_q1, 0, sizeof(cl_mem), &d_q));
@@ -166,7 +168,7 @@ int main(int argc, char const *argv[])
     }
 #endif
 
-    out1(meqn, mbc, mx, xlower, dx, q, 0.0, iframe, NULL, maux);
+    out1(meqn, mbc, mx, xlower, dx, q, 0.0, iframe, NULL, maux, outdir);
     iframe++;
 
     tout += dtout;
@@ -180,7 +182,16 @@ int main(int argc, char const *argv[])
         clEnqueueCopyBuffer (commands, d_q, d_q_old, 0, 0, sizeof(float)*mtot*meqn, 0, NULL, NULL);
 
         CheckError(clEnqueueNDRangeKernel(commands, k_bc1, 1, NULL, &global, &local, 0, NULL, NULL));
-        CheckError(clEnqueueNDRangeKernel(commands, k_rp1_acoustic, 1, NULL, &global, &local, 0, NULL, NULL));
+        CheckError(clEnqueueNDRangeKernel(commands, k_rp1_euler, 1, NULL, &global, &local, 0, NULL, NULL));
+        // CheckError(clEnqueueReadBuffer(commands, d_s, CL_TRUE, 0, sizeof(float)*mtot, s, 0, NULL, NULL));
+        // std::cout<<std::endl;
+        // for (int i = 4; i < 5 ; ++i){
+        //     std::cout<<"d_s["<<std::setw(2)<<i<<"] = "<<std::setw(5)<<s[i]<<std::endl;
+        // }       
+        // CheckError(clEnqueueReadBuffer(commands, d_amdq, CL_TRUE, 0, sizeof(float)*meqn*mtot, s, 0, NULL, NULL));
+        // for (int i = 4; i < 5 ; ++i){
+        //     std::cout<<"d_amdq["<<std::setw(2)<<i<<"] = "<<std::setw(5)<<s[meqn*i]<<std::endl;
+        // }
         CheckError(clEnqueueNDRangeKernel(commands, k_update_q1, 1, NULL, &global, &local, 0, NULL, NULL));
         
         /* Get the maximum speed by recursively calling k_max_speed kernel */ 
@@ -193,7 +204,7 @@ int main(int argc, char const *argv[])
         }
         CheckError(clEnqueueReadBuffer(commands, d_s, CL_TRUE, 0, sizeof(float), &cfl, 0, NULL, NULL ));
         cfl *= dt/dx;
-        
+        std::cout<<"cfl = "<<cfl<<std::endl;
         /* Choose new time step if variable time step */
         if (cfl > 0){
             dt = std::min(dtmax, dt*cfldesire/cfl);
@@ -211,7 +222,6 @@ int main(int argc, char const *argv[])
         //     // Reject this step => Take a smaller step
 
         // }
-        if (t >= t_final)    break;
 
         /* Read ouput array */
         CheckError(clEnqueueReadBuffer(commands, d_q, CL_TRUE, 0, sizeof(float)*mtot*meqn, q, 0, NULL, NULL));
@@ -223,12 +233,13 @@ int main(int argc, char const *argv[])
                      <<",  u["<<std::setw(2)<<i<<"] = "<<std::setw(5)<<q[2*i+1]<<std::endl;
         }
 #endif
-        if (t > tout)
+        if (t >= tout)
         {
-            out1(meqn, mbc, mx, xlower, dx, q, t, iframe, NULL, maux);
+            out1(meqn, mbc, mx, xlower, dx, q, t, iframe, NULL, maux, outdir);
             iframe++;
             tout += dtout;
         }
+        if (t >= t_final)    break;
     }
 
     clReleaseMemObject(d_q);
@@ -238,13 +249,13 @@ int main(int argc, char const *argv[])
     clReleaseMemObject(d_s);
     clReleaseCommandQueue (commands);
     
-    clReleaseKernel(k_rp1_acoustic);
+    clReleaseKernel(k_rp1_euler);
     clReleaseKernel(k_bc1);
     clReleaseKernel(k_qinit);
     clReleaseKernel(k_update_q1);
     clReleaseKernel(k_max_speed);
 
-    clReleaseProgram(p_rp1_acoustic);
+    clReleaseProgram(p_rp1_euler);
     clReleaseProgram(p_bc1);
     clReleaseProgram(p_qinit);
     clReleaseProgram(p_update_q1);
