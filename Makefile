6f84f82e71f612241e7eaaa97cb19abc3d0cea60
MelodyShih <yoyoshih13@gmail.com>
2016-12-16 11:57:22 -0500

    add kernel info to see how many registers are used in each kernel

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ acoustic: acoustic.o
 	g++ -o $@ $< -L/usr/local/cuda-7.5/lib64 -lOpenCL
 	rm *.o
 acoustic.o: acoustic.cpp
-	g++  -c $< -I/usr/local/cuda-7.5/include
+	g++ -c $< -I/usr/local/cuda-7.5/include
 
 acoustic_v2: acoustic_v2.o
 	g++ -o $@ $< -L/usr/local/cuda-7.5/lib64 -lOpenCL
@@ -14,6 +14,18 @@ euler_v2: euler_v2.o
 	g++ -o $@ $< -L/usr/local/cuda-7.5/lib64 -lOpenCL
 	rm *.o 
 euler_v2.o: euler_v2.cpp
+	g++ -c $< -I/usr/local/cuda-7.5/include 
+
+device_info: device_info.o
+	g++ -o $@ $< -L/usr/local/cuda-7.5/lib64 -lOpenCL
+	rm *.o
+device_info.o: device_info.cpp
+	g++ -c $< -I/usr/local/cuda-7.5/include
+
+kernel_info: kernel_info.o
+	g++ -o $@ $< -L/usr/local/cuda-7.5/lib64 -lOpenCL
+	rm *.o
+kernel_info.o: kernel_info.cpp
 	g++ -c $< -I/usr/local/cuda-7.5/include
 
 clean:
---
+++ kernel_info.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <cassert>
+#include <fstream>
+#include <time.h>
+
+#include <CL/cl.h>
+#include "helper.h"
+
+int main(int argc, char const *argv[])
+{
+    // -------------------------------------------------------------------------------//
+    //    This code is used to get the information of how many registers are used     //
+    //    in the specified kernel                                                     //
+    //    Usage:                                                                      //
+    //         ./kernel_info <kernel's file name>                                     //
+    //         e.g. ./kernel_info euler_qinit.cl                                      //
+    //                                                                                //
+    //                                                                                //
+    //    Note: The compiler is doing some caching and won't compile the code again   //
+    //          if it isn't necessary, which causes that the information won't print  //
+    //          out sometime. A possible solution may be changing the kernel's name   //
+    //   (discussed in:                                                               //
+    //        https://devtalk.nvidia.com/default/topic/472539/bug-cl-nv-verbose/)     //
+    // -------------------------------------------------------------------------------//
+    
+    cl_int            err;
+    cl_platform_id    platform = GetPlatform(0); 
+    cl_device_id      device = GetDevice(platform, 0);
+    cl_context        context;
+    cl_command_queue  commands;
+    cl_program        program;
+
+    if (argc < 2 )
+    {
+        std::cout<<"Kernel's name missed"<<std::endl;
+        return 0;
+    }
+
+    std::string s1= "Kernel/";
+    std::string s2= argv[1];
+    s1.append(s2);
+    std::cout<<s1<<std::endl;
+    cl_char *buffer;
+    size_t len;
+
+    context = clCreateContext(NULL, 1, &device, NULL, NULL, &err);
+    commands = clCreateCommandQueue (context, device, 0, &err);
+
+    /* Create program, kernel from source */
+    program = CreateProgram(LoadKernel (s1.c_str()), context);
+    err     = clBuildProgram(program, 1, &device, "-cl-nv-verbose", NULL, NULL);
+    
+    clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, 0, NULL, &len);
+    buffer = new cl_char[len];
+    clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, len, buffer, NULL);
+    std::cout<<buffer<<std::endl;
+
+    delete [] buffer;
+    clReleaseProgram(program);
+    clReleaseCommandQueue(commands);
+    clReleaseContext(context);
+    clReleaseDevice(device);
+
+
+    return 0;
+}
